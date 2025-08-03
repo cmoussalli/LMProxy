98cdb1d9cdd4ba38a42faeac99e5f25f8bbaacae
Caesar Moussalli <cesar_moussalli@hotmail.com>
2025-08-04 01:43:11 +0400

    Init Projects

---
+++ MLProxy/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MLProxy.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("api/test")]
+        public IActionResult Test()
+        {
+            return Ok("Hello from MLProxy");
+        }
+    }
+}
---
+++ MLProxy/Program.cs
@@ -0,0 +1,40 @@
+using MLProxy.Components;
+
+namespace MLProxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
